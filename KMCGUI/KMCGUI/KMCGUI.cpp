ae587053d1965d65c1f707dc3faa698ae3ef912d
Mateusz Ch <mat.chojnacki33@gmail.com>
2022-10-02 23:39:08 +0200

    Rename other variables/symbols and methods

--- KMCGUI/KMCGUI/KMCGUI.cpp
+++ KMCGUI/KMCGUI/KMCGUI.cpp
@@ -14,23 +14,23 @@ KMCGUI::KMCGUI(QWidget *parent)
     ui.threadsSlider->setMinimum(1);
     ui.threadsSlider->setMaximum(64);
     ui.threadsSlider->setValue(std::thread::hardware_concurrency());
-    ui.->setMinimum(1);
-    ui.horizontalSlider_3->setMaximum(128);
-    ui.horizontalSlider_3->setValue(12);
-    ui.horizontalSlider_4->setMinimum(5);
-    ui.horizontalSlider_4->setMaximum(11);
-    ui.horizontalSlider_4->setValue(9);
-    ui.checkBox_2->setChecked(1);
-    connect(ui.chooseButton, SIGNAL(clicked()), this, SLOT(on_pushButton_2_clicked()));
-    connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
+    ui.GBSlider->setMinimum(1);
+    ui.GBSlider->setMaximum(128);
+    ui.GBSlider->setValue(12);
+    ui.signatureLengthSlider->setMinimum(5);
+    ui.signatureLengthSlider->setMaximum(11);
+    ui.signatureLengthSlider->setValue(9);
+    ui.canoncialKmersCheckBox->setChecked(1);
+    connect(ui.chooseButton, SIGNAL(clicked()), this, SLOT(on_chooseButton_clicked()));
+    connect(ui.runButton, SIGNAL(clicked()), this, SLOT(on_runButton_clicked()));
     connect(ui.kmerLengthSlider, SIGNAL(ui.horizontalSlider->valueChanged()), this,
         SLOT(on_horizontalSlider_valueChanged()()));
     connect(ui.threadsSlider, SIGNAL(ui.threadsSlider->valueChanged()), this,
         SLOT(on_threadsSlider_valueChanged()()));
-    connect(ui.kmerLengthSlider, SIGNAL(ui.horizontalSlider_3->valueChanged()), this,
-        SLOT(on_horizontalSlider_3_valueChanged()()));
-    connect(ui.kmerLengthSlider, SIGNAL(ui.horizontalSlider_4->valueChanged()), this,
-        SLOT(on_horizontalSlider_4_valueChanged()()));
+    connect(ui.GBSlider, SIGNAL(ui.GBSlider->valueChanged()), this,
+        SLOT(on_GBSlider_valueChanged()()));
+    connect(ui.signatureLengthSlider, SIGNAL(ui.signatureLengthSlider->valueChanged()), this,
+        SLOT(on_signatureLengthSlider_valueChanged()()));
 }
 
 KMCGUI::~KMCGUI()
@@ -38,25 +38,25 @@ KMCGUI::~KMCGUI()
 
 void KMCGUI::on_kmerLengthSlider_valueChanged()
 {
-    ui.label_10->setText(QString::number(ui.kmerLengthSlider->value()));
+    ui.kmerLengthValue->setText(QString::number(ui.kmerLengthSlider->value()));
 }
 
 void KMCGUI::on_threadsSlider_valueChanged()
 {
-    ui.label_12->setText(QString::number(ui.threadsSlider->value()));
+    ui.threadsValue->setText(QString::number(ui.threadsSlider->value()));
 }
 
-void KMCGUI::on_horizontalSlider_3_valueChanged()
+void KMCGUI::on_GBSlider_valueChanged()
 {
-    ui.label_16->setText(QString::number(ui.horizontalSlider_3->value()));
+    ui.maxGBValue->setText(QString::number(ui.GBSlider->value()));
 }
 
-void KMCGUI::on_horizontalSlider_4_valueChanged()
+void KMCGUI::on_signatureLengthSlider_valueChanged()
 {
-    ui.label_20->setText(QString::number(ui.horizontalSlider_4->value()));
+    ui.signatureLengthValue->setText(QString::number(ui.signatureLengthSlider->value()));
 }
 
-void KMCGUI::on_pushButton_2_clicked()
+void KMCGUI::on_chooseButton_clicked()
 {
     QFileDialog* fileDialog = new QFileDialog();
     fileNames = fileDialog->getOpenFileNames(this, tr("Open Files"),
@@ -64,11 +64,15 @@ void KMCGUI::on_pushButton_2_clicked()
         tr("Fasta files (*.fq *.fastq)"));
 }
 
-void KMCGUI::on_pushButton_clicked()
+void KMCGUI::on_runButton_clicked()
 {
+    ui.errorMessageBox->setText("");
+    ui.totalKmersValue->setText("Wait for result...");
+    ui.totalUniqueKmersValue->setText("Wait for result...");
+
     if (fileNames.count() == 0)
     {
-        ui.label_4->setText("No files choosed");
+        ui.errorMessageBox->setText("No files choosed");
     }
     else 
     {
@@ -87,11 +91,11 @@ void KMCGUI::on_pushButton_clicked()
             stage1Params
                 .SetKmerLen(ui.kmerLengthSlider->value())
                 .SetNThreads(ui.threadsSlider->value())
-                .SetMaxRamGB(ui.horizontalSlider_3->value())
-                .SetSignatureLen(ui.horizontalSlider_4->value())
-                .SetHomopolymerCompressed(ui.checkBox->isChecked())
-                .SetCanonicalKmers(ui.checkBox_2->isChecked())
-                .SetRamOnlyMode(ui.checkBox_3->isChecked())
+                .SetMaxRamGB(ui.GBSlider->value())
+                .SetSignatureLen(ui.signatureLengthSlider->value())
+                .SetHomopolymerCompressed(ui.homopomylerCompressionCheckBox->isChecked())
+                .SetCanonicalKmers(ui.canoncialKmersCheckBox->isChecked())
+                .SetRamOnlyMode(ui.ramModeCheckBox->isChecked())
                 .SetInputFiles(stringFileNames);
 
             auto stage1Result = runner.RunStage1(stage1Params);
@@ -107,13 +111,13 @@ void KMCGUI::on_pushButton_clicked()
             std::string totalKmers = std::to_string(stage2Result.nTotalKmers);
             std::string uniqueKmers = std::to_string(stage2Result.nUniqueKmers);
 
-            ui.label_4->setText(QString::fromUtf8(totalKmers.c_str()));
-            ui.label_5->setText(QString::fromUtf8(uniqueKmers.c_str()));
-
+            ui.totalKmersValue->setText(QString::fromUtf8(totalKmers.c_str()));
+            ui.totalUniqueKmersValue->setText(QString::fromUtf8(uniqueKmers.c_str()));
+            
         }
         catch (const std::exception& e)
         {
-            ui.label->setText(QString::fromUtf8(e.what()));
+            ui.errorMessageBox->setText(QString::fromUtf8(e.what()));
         }
     }
 }
